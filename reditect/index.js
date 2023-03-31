1838e003ff503a25b932c326f1085fd137f79a16
lranatunga <rmlik.ranatunga@gmail.com>
2023-03-31 16:55:15 +0100

    page redirection

---
+++ reditect/index.js
@@ -0,0 +1,7 @@
+const redirectBtn = document.querySelector("button");
+redirectBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+  window.location.href = "https://getbootstrap.com";
+});
+
+  
